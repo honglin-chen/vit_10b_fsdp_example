fe535860f71e36e4a86c059b17cf20e65a2c177e
Honglin Chen <53631145+honglin-chen@users.noreply.github.com>
2023-06-01 14:40:49 -0700

    Update train_v2.sh

--- train_v2.sh
+++ train_v2.sh
@@ -8,7 +8,7 @@ python3 -u ~/vit_10b_fsdp_example/run_vit_training.py \
   --fake_data \
   --ckpt_dir ${SAVE_DIR} \
   --image_size 224 \
-  --patch_size 14 \
+  --patch_size 4 \
   --embed_dim 1024 \
   --mlp_ratio 4.0 \
   --num_heads 32 \
